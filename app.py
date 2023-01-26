f1b8e0c0fd4965707020e81a2ac0318e49f570dd
MaarkNassef <maark.nassef@gmail.com>
2023-01-26 10:53:50 +0200

    New update: App doesn't save a copy of the file.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, redirect, render_template, url_for, send_file, flash
 from datetime import datetime
 import hashlib
-from convert import convert_file
-import os
+from convert import convert
+import io
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Powerful secret key"
@@ -18,35 +18,16 @@ def upload_file():
             flash("Choose a valid PDF file")
             return render_template('index.html')
         else:
-            filename=hashlib.md5(str(datetime.now()).encode()).hexdigest()
-            file.save(f'temp/upload/{filename}.pdf')
-            return redirect(url_for('convert', filename=filename))
+            content = file.read()
+            result = convert(content=content)
+            new_filename = hashlib.md5(str(datetime.now()).encode('utf-8')).hexdigest()
+            return send_file(
+                io.BytesIO(result.tobytes()),
+                mimetype='application/pdf',
+                as_attachment=True,
+                download_name=f'{new_filename}.pdf'
+            )
     return render_template('index.html')
-
-@app.route('/convert/<string:filename>')
-def convert(filename: str):
-    new_file_path = convert_file(filename)
-    try:
-        os.remove(os.path.relpath(f'temp/upload/{filename}.pdf'))
-    except:
-        pass
-    return render_template('convert.html', filepath=new_file_path)
-
-
-@app.route('/download/<path:filepath>')
-def download_file(filepath):
-    try:
-        return send_file(filepath, as_attachment=True)
-    except Exception as e:
-        return str(e)
-    
-@app.route('/remove/<path:filepath>')
-def remove(filepath: str):
-    try:
-        os.remove(os.path.relpath(filepath))
-    except:
-        pass
-    return redirect('/')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- convert.py
+++ convert.py
@@ -1,19 +1,15 @@
 import fitz
 
-def convert_file(path: str):
-    pdf_doc = fitz.open(f"temp/upload/{path}.pdf")
+def convert(content):
+    pdf_document = fitz.open(stream = content, filetype = 'pdf')
+    img_pdf = fitz.open()
     images = []
-    for page_num in range(pdf_doc.page_count):
-        page = pdf_doc[page_num]
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document[page_num]
         pix = page.get_pixmap(matrix=fitz.Matrix(2,2))
         images.append(pix.tobytes())
         
-    output_pdf = fitz.open()
-
     for image in images:
-        pdf_page = output_pdf.new_page()
+        pdf_page = img_pdf.new_page()
         pdf_page.insert_image(pdf_page.rect, stream=image, xref=0)
-
-    output_pdf.save(f"temp/download/{path}.pdf")
-
-    return f"temp/download/{path}.pdf"
+    return img_pdf
